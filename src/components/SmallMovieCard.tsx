ddf77ec5698dae0055fdde2f667c6f5b82d671a3
a.masalov <delsol@bk.ru>
2021-05-02 09:52:22 +0300

    mocks

---
+++ src/components/SmallMovieCard.tsx
@@ -0,0 +1,15 @@
+export function SmallMovieCard(props) {
+  const { name, href, imgSrc } = props
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={imgSrc} alt={name} width="280" height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href={href}>
+          {name}
+        </a>
+      </h3>
+    </article>
+  )
+}
---
+++ src/mocks/films.ts
@@ -0,0 +1,32 @@
+export const films = [
+  {
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+    url: 'movie-page.html',
+    imgSrc: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  },
+  {
+    name: 'Bohemian Rhapsody',
+    url: 'movie-page.html',
+    imgSrc: 'img/bohemian-rhapsody.jpg',
+  },
+  {
+    name: 'Macbeth',
+    url: 'movie-page.html',
+    imgSrc: 'img/macbeth.jpg',
+  },
+  {
+    name: 'Aviator',
+    url: 'movie-page.html',
+    imgSrc: 'img/aviator.jpg',
+  },
+  {
+    name: 'We need to talk about Kevin',
+    url: 'movie-page.html',
+    imgSrc: 'img/we-need-to-talk-about-kevin.jpg',
+  },
+  {
+    name: 'What We Do in the Shadows',
+    url: 'movie-page.html',
+    imgSrc: 'img/what-we-do-in-the-shadows.jpg',
+  },
+]
