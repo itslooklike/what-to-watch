878ec7d6a84664372a0b25e82eeec2b29aba0aa5
amasalov <delsol@bk.ru>
2021-05-06 23:47:54 +0300

    add typing

--- src/store/hocs/withAuth.tsx
+++ src/store/hocs/withAuth.tsx
@@ -3,19 +3,21 @@ import { useRouter } from 'next/router'
 import { useMobxStores } from '~/store'
 import { isServer } from '~/utils/isServer'
 
-export const withAuth = (WrappedComponent: any) => (props: any) => {
-  const router = useRouter()
-  const { userStore } = useMobxStores()
+export function withAuth<T>(WrappedComponent: React.ComponentType<T>) {
+  return (props: T) => {
+    const router = useRouter()
+    const { userStore } = useMobxStores()
 
-  if (!isServer) {
-    if (!userStore.user) {
-      router.push('/login')
-      return null
+    if (!isServer) {
+      if (!userStore.user) {
+        router.push('/login')
+        return null
+      }
+
+      /* eslint-disable-next-line react/jsx-props-no-spreading */
+      return <WrappedComponent {...props} />
     }
 
-    /* eslint-disable-next-line react/jsx-props-no-spreading */
-    return <WrappedComponent {...props} />
+    return null
   }
-
-  return null
 }
