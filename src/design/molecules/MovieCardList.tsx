003083c1cec648700c8a69e37ae714382000d561
amasalov <delsol@bk.ru>
2021-05-07 23:43:47 +0300

    update grid

--- src/design/molecules/MovieCardList.tsx
+++ src/design/molecules/MovieCardList.tsx
@@ -6,9 +6,8 @@ import type { IFilm } from '~/store/FilmsStore'
 
 const Root = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
   gap: 20px;
-  align-items: flex-start;
 `
 
 type TProps = {
