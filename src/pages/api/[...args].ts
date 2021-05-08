4b4e2ab2618119fe1db353251c06bc0b94186b48
amasalov <delsol@bk.ru>
2021-05-08 13:23:16 +0300

    add proxy

---
+++ src/pages/api/[...args].ts
@@ -0,0 +1,11 @@
+import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware'
+import { apiUrl } from '~/utils/config'
+
+const proxy = createProxyMiddleware('/api', {
+  target: apiUrl,
+  changeOrigin: true,
+  pathRewrite: { '^/api': '/wtw' },
+  onProxyReq: fixRequestBody,
+})
+
+export default proxy
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { apiUrl } from '~/utils/config'
+import { isServer } from '~/utils/env'
 
 export type { AxiosError } from 'axios'
 
 export const api = axios.create({
-  baseURL: `${apiUrl}/wtw`,
+  // baseURL: `${apiUrl}/wtw`, // NO-PROXY use
+  baseURL: isServer ? `${apiUrl}/wtw` : `/api`,
   timeout: 5_000,
   withCredentials: true,
 })
